1a9151a08a4bd5a997229a97ffbf83812c2025d3
Pavel Kepka <pkepka@blueberryapps.com>
2017-07-04 12:50:33 +0200

    Working preview

--- src/App.js
+++ src/App.js
@@ -4,42 +4,75 @@ const hasGetUserMedia = () => {
   if (!navigator) {
     return false;
   }
-  return !!(
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia
-  );
+  return !!navigator.mediaDevices.getUserMedia;
 };
+
 class ImageCapture extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       hasGetUserMedia: false,
     };
   }
 
   componentDidMount() {
+    const isEnabled = hasGetUserMedia();
     this.setState({
-      hasGetUserMedia: hasGetUserMedia(),
+      hasGetUserMedia: isEnabled,
     });
+    if (isEnabled) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          this.video.srcObject = stream;
+          this.video.play();
+        })
+        .catch((err) => {
+          console.error('An error occured! ', err); // eslint-disable-line no-console
+        });
+    }
   }
 
+  takePicture(event) {
+    event.preventDefault();
+    this.canvas.getContext('2d')
+      .drawImage(this.video, 0, 0, 300, 300, 0, 0, 300, 300);
+    const img = this.canvas.toDataURL('image/png');
+    this.props.takePicture(img);
+  };
+
   render() {
-    console.log(this.state);
     return (
-      <div>{`${this.state.hasGetUserMedia}`}</div>
+      <div>
+        <video ref={(video) => { this.video = video; }} width="300" height="300" />
+        <button ref={(button) => { this.button = button; }} onClick={this.takePicture.bind(this)}>Take picture</button>
+        <canvas width="300" height="300" ref={(canvas) => { this.canvas = canvas; }} style={{ display: 'none' }}/>
+        <p>{`${this.state.hasGetUserMedia}`}</p>
+      </div>
     );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: '',
+    };
+  }
+
+  takePicture(image) {
+    this.setState({ src: image });
+  }
+
   render() {
     return (
       <div className="App">
         <div>----------</div>
-        <ImageCapture />
+        <ImageCapture takePicture={this.takePicture.bind(this)} />
         <div>----------</div>
+        <div>
+          <img src={this.state.src} width="300" height="300" />
+        </div>
       </div>
     );
   }
