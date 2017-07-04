584f8b64c4ba21fb5764ba4b0d4ba4d9665dda40
Pavel Kepka <pkepka@blueberryapps.com>
2017-07-04 08:00:54 +0200

    Image capture component sceleton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const hasGetUserMedia = () => {
+  if (!navigator) {
+    return false;
+  }
+  return !!(
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
+  );
+};
+class ImageCapture extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasGetUserMedia: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      hasGetUserMedia: hasGetUserMedia(),
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>{`${this.state.hasGetUserMedia}`}</div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>----------</div>
+        <ImageCapture />
+        <div>----------</div>
       </div>
     );
   }
